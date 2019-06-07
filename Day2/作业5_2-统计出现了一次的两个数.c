07912abc2c5f40db9e3c0046e79716a5d475d78f
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-07 10:52:06 +0800

    Create 作业5_2-统计出现了一次的两个数.c

---
+++ Day2/作业5_2-统计出现了一次的两个数.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() {
+	int a[] = { 2,3,2,4,4,5 };
+	int x = 0;
+	for (int i = 0; i < 6; ++i) {
+		x ^= a[i];
+	}	//得到两个不相同的数的异或结果
+	int y = x & -x;	//最低位为1的那个数
+	int a1[6] = { 0 }, a0[6] = { 0 };
+	int i1 = 0, i0 = 0;
+	//分成两堆
+	for (int i = 0; i < 6; ++i) {
+		if (a[i] & y) {
+			a1[i1] = a[i];
+			++i1;
+		}
+		else {
+			a0[i0] = a[i];
+			++i0;
+		}
+	}
+	printf("出现了一次的那两个数为:");
+	//分别求两个数
+	x = 0;
+	for (int i = 0; i < 6; ++i) {
+		x ^= a1[i];
+	}
+	printf("%d ", x);
+	x = 0;
+	for (int i = 0; i < 6; ++i) {
+		x ^= a0[i];
+	}
+	printf("%d\n", x);
+	system("pause");
+}
