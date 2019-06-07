3a6f33b93bbb7b53cd4dd878a4489ffaea0bdd58
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-07 10:47:20 +0800

    Create 作业3_1-十进制转二进制.c

---
+++ Day2/作业3_1-十进制转二进制.c
@@ -0,0 +1,41 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+
+int main() {
+	int ten = 0, i = 31, flag = 0, under0 = 0;
+	int two[32] = {0};
+	while (fflush(stdin), scanf("%d",&ten) != EOF) {	//输入十进制数
+		i = 31;	//每次循环都必须初始化
+		flag = 0;
+		under0 = 0;
+		for (int j = 0; j < 32; ++j) two[j] = 0;
+		if (ten < 0) {
+			under0 = 1;
+			ten = -ten;
+		}
+		while (ten >= 1) {
+			two[i] = ten % 2;
+			ten /= 2;
+			--i;
+		}
+		if (under0) {	//若输入的值为负数
+			for (int j = 0; j < 32; ++j) {
+				if (two[j]) two[j] = 0;
+				else two[j] = 1;
+			}	//取反
+			++two[31];	//最后一位加一
+			for (int j = 31; j >= 0; --j) {
+				if (two[j] == 2) {
+					two[j] = 0;
+					if(j>0) ++two[j - 1];
+				}
+			}	//进位
+		}
+		for (int j = 0; j < 32; ++j) {	//输出二进制值
+			if (two[j] != 0) flag = 1;
+			if (flag) printf("%d", two[j]);
+		}
+		putchar('\n');
+	}
+	system("pause");
+}
