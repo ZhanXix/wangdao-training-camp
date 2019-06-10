737021c0a2c4237cc75d1916d2736b7f5e31271e
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-10 11:14:27 +0800

    Add files via upload

--- Day6/函数的嵌套调用.c
+++ Day6/函数的嵌套调用.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <setjmp.h>
 
-jmp_buf envbuf;
-void b()
+void b(jmp_buf envbuf)
 {
 	printf("I am func b.\n");
 	longjmp(envbuf,5);//这个后面的参数代表setjump的返回值，不能填0
 }
 
-void a()
+void a(jmp_buf envbuf)
 {
 	printf("Befor b, I am func a.\n");
-	b();
+	b(envbuf);
 	printf("After b, I am func a.\n");
 }
 
 int main()
 {
+	jmp_buf envbuf;
 	int ret;
 	ret = setjmp(envbuf);//longjmp跳回后ret就不是0了（是5）
 	if (ret == 0) {
-		a();
+		a(envbuf);
 	}
 	return 0;
 }
