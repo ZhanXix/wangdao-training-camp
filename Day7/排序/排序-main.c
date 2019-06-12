572e7abbc250752142c1e881db68e2e9222f68bf
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-12 09:48:04 +0800

    change

---
+++ Day7/排序/排序-main.c
@@ -0,0 +1,31 @@
+#include "ÅÅĞò-sort.h"
+
+int cmp(const void* left, const void* right)
+{
+	int* p1 = (int*)left;
+	int* p2 = (int*)right;
+	return *p1 - *p2;
+}
+int main()
+{
+	int i;
+	int arr[N];
+	time_t start, end;
+	srand(time(NULL));
+	//time(NULL)·µ»Ø¾àÀë1970.1.1µÄÃëÊı
+	for (i = 0; i < 10; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+	arrPrint(arr);
+	start = time(NULL);
+	//arrSelect(arr);
+	//arrInsert(arr);
+	//arrShell(arr);
+	arrQuick(arr, 0, N-1);
+	//qsort(arr, N, sizeof(int), cmp);
+	end = time(NULL);
+	arrPrint(arr);
+	printf("use time = %ds\n", end - start);
+	system("pause");
+}
