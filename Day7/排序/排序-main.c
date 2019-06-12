0372714db38e3795f555b55f353f6be86b3b49ea
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-12 11:50:35 +0800

    update sort

--- Day7/排序/排序-main.c
+++ Day7/排序/排序-main.c
@@ -9,25 +9,28 @@ int cmp(const void* left, const void* right)
 int main()
 {
 	int i;
-	int arr[N];
+	//int arr[N];
+	//N过大时不能用栈空间，windows下单个函数的栈空间只有1M
+	int* arr = (int*)malloc(N * sizeof(int));
 	time_t start, end;
 	srand(time(NULL));
 	//time(NULL)返回距离1970.1.1的秒数
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < N; i++)
 	{
 		arr[i] = rand() % 100;
 	}
-	arrPrint(arr);
+	printf("rand %d complete.\n", N);
+	//arrPrint(arr);	//N过大时就不合适再输出每个数了
 	start = time(NULL);
 	//arrSelect(arr);
 	//arrInsert(arr);
 	//arrShell(arr);
-	//arrQuick(arr, 0, N-1);
+	//arrQuick(arr, 0, N-1);	//N过大时递归过多，造成溢出
+	//qsort(arr, N, sizeof(int), cmp);	//qsort会直接先分配空间，不会造成我们写的快排的溢出问题
 	//arrHeap(arr);
 	arrCount(arr);
-	//qsort(arr, N, sizeof(int), cmp);
 	end = time(NULL);
-	arrPrint(arr);
+	//arrPrint(arr);	//N过大时就不合适再输出每个数了
 	printf("use time = %ds\n", end - start);
 	system("pause");
 }
