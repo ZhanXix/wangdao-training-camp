0847b0e67bc6c253b987d8e408383c6fadd504dd
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-13 18:00:54 +0800

    Create fopen打开文件.c

---
+++ Day9/fopen打开文件.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE* fp;
+	char c;
+	int ret;
+	char buf[5000];
+	fp = fopen("file.txt‪","r+");	//可以是相对路径（相对于当前目录的路径），也可以是绝对路径（从盘符开始的路径）
+	if (NULL == fp)
+	{
+		perror("fopen");
+		goto error;
+	}
+	//while ((c = fgetc(fp)) != EOF)
+	//{
+	//	putchar(c);
+	//}
+	//c = 'H';
+	//ret=fputc(c, fp);
+	//if (EOF == ret)
+	//{
+	//	perror("fputc");
+	//	goto error;
+	//}
+	while (memset(buf,0, sizeof(buf)),(fgets(buf, sizeof(buf), fp)) != NULL)//读一行
+	{
+		puts(buf);
+	}
+	fclose(fp);
+error:
+	system("pause");
+}
