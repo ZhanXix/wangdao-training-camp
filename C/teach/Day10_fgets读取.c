1ae5f146f83e1bfc803e59f576814c1087cd4a6e
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-14 11:38:25 +0800

    Update Day10_fgets读取.c

--- C/teach/Day10_fgets读取.c
+++ C/teach/Day10_fgets读取.c
@@ -20,8 +20,15 @@ int main(int argc, char* argv[])	//参数的数量、具体参数（本身的exe程序算第一个参
 	}
 	while (memset(buf, 0, sizeof(buf)), (fgets(buf, sizeof(buf), fp)) != NULL)
 	{
-		buf[0] -= 32;
-		fseek(fp, -strlen(buf), SEEK_CUR);
+		buf[0] -= 32;	//把首字母改为大学
+		if ('\n' == buf[strlen(buf) - 1])
+		{
+			fseek(fp, -strlen(buf) - 1, SEEK_CUR);
+		}
+		else
+		{
+			fseek(fp, -strlen(buf), SEEK_CUR);
+		}
 		fputs(buf, fp);
 		fseek(fp, 0, SEEK_CUR);
 	}
