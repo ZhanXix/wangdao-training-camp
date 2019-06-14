2a9b964d1fbffb5cc1a5c2c1249e317fc3663d08
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-14 11:36:19 +0800

    update fseek.c

--- C/teach/Day10_fgets读取.c
+++ C/teach/Day10_fgets读取.c
@@ -2,31 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-//掌握main函数的传参
 int main(int argc, char* argv[])	//参数的数量、具体参数（本身的exe程序算第一个参数）
 {
 	int i;
-	char c;
+	char buf[1024];
 	FILE* fp;
-	//for (i = 0; i < argc; i++)	//输出参数
-	//{
-	//	puts(argv[i]);
-	//}
+	if (NULL == argv[1])
+	{
+		printf("error args\n");
+		goto error;
+	}
 	fp = fopen(argv[1], "r+");
 	if (NULL == fp)
 	{
 		perror("fopen");
 		goto error;
 	}
-	while ((c = fgetc(fp)) != EOF)
+	while (memset(buf, 0, sizeof(buf)), (fgets(buf, sizeof(buf), fp)) != NULL)
 	{
-		if (c >= 'A' && c <= 'Z')
-		{
-			c += 32;
-			fseek(fp, -1, SEEK_CUR);
-			fputc(c, fp);
-		}
-		fseek(fp, 0, SEEK_CUR);	//写读之间必须刷新位置指针
+		buf[0] -= 32;
+		fseek(fp, -strlen(buf), SEEK_CUR);
+		fputs(buf, fp);
+		fseek(fp, 0, SEEK_CUR);
 	}
 	fclose(fp);
 error:
