c283e4931497b1c77d37e4e821a47cf6fd14d211
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-14 10:24:21 +0800

    rename homework

--- C/teach/Day10_fwrite与fread.c
+++ C/teach/Day10_fwrite与fread.c
@@ -5,7 +5,7 @@
 int main()
 {
 	FILE* fp;
-	char str[10] = "20000";
+	char str[20] = "hello\nworld";
 	int val = 20000;	//20 4e 00 00   写整型、浮点型是无法所见即所得的
 	int ret;
 	fp = fopen("file.txt", "r+");
@@ -14,10 +14,19 @@ int main()
 		perror("fopen");
 		goto error;
 	}
-	//ret = fwrite(str, sizeof(char), strlen(str), fp);
+	val = strlen(str);
+	ret = fwrite(str, sizeof(char), strlen(str), fp);
+	ret = fseek(fp, -12, SEEK_CUR);	//由于文本模式中\n被存为\r\n，所以这里偏移12个字符
+	if (ret != 0)
+	{
+		perror("fseek");
+		goto error;
+	}
 	//ret = fwrite(&val, sizeof(int), 1, fp);	
-	val = 0;
-	ret = fread(&val, sizeof(int), 1, fp);
+	//val = 0;
+	//ret = fread(&val, sizeof(int), 1, fp);
+	memset(str, 0, sizeof(str));
+	ret = fread(str, sizeof(char), sizeof(str), fp);
 	fclose(fp);
 error:
 	system("pause");
