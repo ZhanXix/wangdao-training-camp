1b33cca66ffab337d5c00e54d9b0c3804fbc4c40
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-14 14:43:01 +0800

    update fprintf&fscanf

--- C/teach/Day10_fprintf与fscanf.c
+++ C/teach/Day10_fprintf与fscanf.c
@@ -2,25 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct 
+typedef struct
 {
 	int num;
 	float score;
 	char name[20];
 }Student_t;
 
-int main(int argc,char *argv[])
+int main(int argc, char* argv[])
 {
 	Student_t s = { 1001,98.5,"lele" };
 	FILE* fp;
+	int ret;
 	if (argc != 2)
 	{
 		printf("error args\n");
 		goto error;
 	}
 	fp = fopen(argv[1], "r+");
-	fprintf(fp,"%d %5.2f %s\n", s.num, s.score, s.name);
+	//fprintf(fp,"%d %5.2f %s\n", s.num, s.score, s.name);
 	//fprintf(stdout, "%d %5.2f %s\n", s.num, s.score, s.name);  标准输出，和printf一样会打印到屏幕上
+	while (memset(&s, 0, sizeof(Student_t)), fscanf(fp, "%d%f%s", &s.num, &s.score, s.name) != EOF)
+	{
+		printf("%d %5.2f %s\n", s.num, s.score, s.name);
+	}
+	fclose(fp);
 error:
 	system("pause");
 }
