7f3cdfb87fc698c14c3521c3a653d669d3e38c4a
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-20 17:06:07 +0800

    change

---
+++ Linux/teach/day4_opendir/rewinddir.c
@@ -0,0 +1,21 @@
+#include <func.h>
+
+int main(int args,char *argv[])
+{
+    ARGS_CHECK(args,2);
+    DIR* dir;
+    dir=opendir(argv[1]);
+    ERROR_CHECK(dir,NULL,"opendir");
+    struct dirent *p;
+    off_t pos;
+    while(p=readdir(dir))
+    {
+        printf("ino=%ld len=%d type=%d filename=%s\n",p->d_ino,p->d_reclen,p->d_type,p->d_name);
+    }
+    rewinddir(dir);
+    printf("--------------------------------\n");
+    p=readdir(dir);
+    printf("ino=%ld len=%d type=%d filename=%s\n",p->d_ino,p->d_reclen,p->d_type,p->d_name);
+    closedir(dir);
+    return 0;
+}
