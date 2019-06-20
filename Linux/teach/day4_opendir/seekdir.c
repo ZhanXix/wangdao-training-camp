d7aadfe0c1e661fcde1aacd4b55c1c3d8ca2d7aa
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-20 16:39:07 +0800

    create seekdir

---
+++ Linux/teach/day4_opendir/seekdir.c
@@ -0,0 +1,22 @@
+#include <func.h>
+
+int main(int args,char *argv[])
+{
+    ARGS_CHECK(args,2);
+    DIR* dir;
+    dir=opendir(argv[1]);
+    ERROR_CHECK(dir,NULL,"opendir");
+    struct dirent *p;
+    off_t pos;
+    while(p=readdir(dir))
+    {
+        printf("ino=%ld len=%d type=%d filename=%s\n",p->d_ino,p->d_reclen,p->d_type,p->d_name);
+        if(!strcmp(p->d_name,"a.out"))
+        {
+            pos=telldir(dir);
+        }
+    }
+    seekdir(dir,pos);
+    closedir(dir);
+    return 0;
+}
