fd7d657a1b9259820501080180b589f2706e1b53
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-20 15:05:16 +0800

    Create opendir.c

---
+++ Linux/teach/day4_opendir/opendir.c
@@ -0,0 +1,16 @@
+#include <func.h>
+
+int main(int args,char *argv[])
+{
+    ARGS_CHECK(args,2);
+    DIR* dir;
+    dir=opendir(argv[1]);
+    ERROR_CHECK(dir,NULL,"opendir");
+    struct dirent *p;
+    while(p=readdir(dir))
+    {
+        printf("ino=%ld len=%d type=%d filename=%s\n",p->d_ino,p->d_reclen,p->d_type,p->d_name);
+    }
+    closedir(dir);
+    return 0;
+}
