b1a1a604f21efad64e0b55e9b867dd2c1675a572
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-20 17:10:50 +0800

    Update stat.c

--- Linux/teach/day4_stat/stat.c
+++ Linux/teach/day4_stat/stat.c
@@ -7,6 +7,6 @@ int main(int argc,char *argv[])
     struct stat buf;
     ret=stat(argv[1],&buf);//传出参数
     ERROR_CHECK(ret,-1,"stat");
-    printf("%x %ld %d %d %ld %s\n",buf.st_mode,buf.st_nlink,buf.st_uid,buf.st_gid,buf.st_size,ctime(&buf.st_mtime));
+    printf("%x %ld %s %s %ld %s\n",buf.st_mode,buf.st_nlink,getpwuid (buf.st_uid)->pw_name,getgrgid(buf.st_gid)->gr_name,buf.st_size,ctime(&buf.st_mtime));
     return 0;
 }
