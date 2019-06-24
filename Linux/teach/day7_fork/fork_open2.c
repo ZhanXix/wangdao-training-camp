352b38cb6f2cd6f17c8d3d8718f31985fed4b137
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-24 16:15:44 +0800

    create fork_open.c

---
+++ Linux/teach/day7_fork/fork_open2.c
@@ -0,0 +1,26 @@
+#include <func.h>
+
+int main(int argc,char *argv[])
+{
+    ARGS_CHECK(argc,2);
+    pid_t pid;
+    int fd;
+    ERROR_CHECK(fd,-1,"open");
+    pid=fork();
+    if(0==pid)
+    {
+        fd=open(argv[1],O_RDWR);
+        printf("I am child,my pid=%d,my ppid=%d\n",getpid(),getppid());
+        write(fd,"niuniu",6);
+    }
+    else
+    {
+        fd=open(argv[1],O_RDWR);
+        printf("I am parent,my pid=%d,my childpid=%d\n",getpid(),pid);
+        lseek(fd,5,SEEK_SET);
+        printf("I am parent,seek success\n");
+        sleep(1);
+    }
+    return 0;
+}
+
