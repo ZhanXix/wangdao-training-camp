073b676ae9807cadbe2be247d2e5a4fd3d893bec
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-25 20:19:48 +0800

    update 8_4

--- Linux/homework/8_5_semaphore_add1000w.c
+++ Linux/homework/8_5_semaphore_add1000w.c
@@ -1,4 +1,4 @@
-//两个进程对一个共享内存各加1000万，查看最后结果
+//两个进程对同一个数据各加1千万，通过使用信号量，最终结果是2千万
 #include <func.h>
 
 int main(int argc,char *argv[])
@@ -9,13 +9,27 @@ int main(int argc,char *argv[])
     int *p=(int*)shmat(shmid,NULL,0);
     ERROR_CHECK(p,(int*)-1,"shmat");
     p[0]=0;
+    int semid;
+    semid=semget(1000,1,IPC_CREAT|0600);
+    ERROR_CHECK(semid,-1,"semget");
+    int ret=semctl(semid,0,SETVAL,1);
+    ERROR_CHECK(ret,-1,"semctl");
+    struct sembuf P,V;
+    P.sem_num=0;
+    P.sem_op=-1;
+    P.sem_flg=SEM_UNDO;
+    V.sem_num=0;
+    V.sem_op=1;
+    V.sem_flg=SEM_UNDO;
     pid_t pid=fork();
     ERROR_CHECK(pid,-1,"fork");
     if(pid>0){//父进程
         int i;
         for(i=0;i<10000000;++i)
         {
+            semop(semid,&P,1);
             ++p[0];
+            semop(semid,&V,1);
         }
         wait(NULL);
         printf("add result=%d\n",p[0]);
@@ -24,7 +38,9 @@ int main(int argc,char *argv[])
         int i;
         for(i=0;i<10000000;++i)
         {
+            semop(semid,&P,1);
             ++p[0];
+            semop(semid,&V,1);
         }
         exit(0);
     }
