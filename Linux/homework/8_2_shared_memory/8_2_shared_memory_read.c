8337d05d9ab38a9eba9b2ded77bd5d56612ba68b
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-25 19:42:28 +0800

    update 8_2

--- Linux/homework/8_2_shared_memory/8_2_shared_memory_read.c
+++ Linux/homework/8_2_shared_memory/8_2_shared_memory_read.c
@@ -3,7 +3,7 @@
 int main(int argc,char *argv[])
 {
     int shmid;
-    shmid=shmget(1000,1<<12,IPC_CREAT|0666);
+    shmid=shmget(1000,1<<12,IPC_CREAT|0600);
     ERROR_CHECK(shmid,-1,"shmget");
     char *p=(char*)shmat(shmid,NULL,0);
     ERROR_CHECK(p,(char*)-1,"shmat");
