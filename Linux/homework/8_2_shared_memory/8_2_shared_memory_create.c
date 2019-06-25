f9216fadaf7c8525fd29cadf602534e73fae6ced
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-25 19:41:41 +0800

    create shared_memory

---
+++ Linux/homework/8_2_shared_memory/8_2_shared_memory_create.c
@@ -0,0 +1,15 @@
+//新建共享内存，连接，然后往共享内存写入How are you,然后另外一个进程连接共享内存，读取数据并打印显示,删除共享内存
+#include <func.h>
+
+int main(int argc,char *argv[])
+{
+    int shmid;
+    shmid=shmget(1000,1<<12,IPC_CREAT|0666);
+    ERROR_CHECK(shmid,-1,"shmget");
+    char *p=(char*)shmat(shmid,NULL,0);
+    ERROR_CHECK(p,(char*)-1,"shmat");
+    strcpy(p,"How are you");
+    while(1);
+    return 0;
+}
+
---
+++ Linux/homework/8_2_shared_memory/8_2_shared_memory_read.c
@@ -0,0 +1,15 @@
+#include <func.h>
+
+int main(int argc,char *argv[])
+{
+    int shmid;
+    shmid=shmget(1000,1<<12,IPC_CREAT|0666);
+    ERROR_CHECK(shmid,-1,"shmget");
+    char *p=(char*)shmat(shmid,NULL,0);
+    ERROR_CHECK(p,(char*)-1,"shmat");
+    printf("已从共享内存中读取数据：%s\n",p);
+    int ret=shmctl(shmid,IPC_RMID,NULL);
+    ERROR_CHECK(ret,-1,"shmctl");
+    return 0;
+}
+
