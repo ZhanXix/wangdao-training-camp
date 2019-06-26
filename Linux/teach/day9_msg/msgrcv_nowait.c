99bfd0031db671a6479c3bbf7e67b49ee57c2384
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-26 15:48:05 +0800

    Create msgrcv_nowait.c

---
+++ Linux/teach/day9_msg/msgrcv_nowait.c
@@ -0,0 +1,19 @@
+#include <func.h>
+typedef struct msgbuf{
+    long mtype;
+    char mtext[64];
+}MSG_T;
+int main(int argc,char *argv[])
+{
+    ARGS_CHECK(argc,2);
+    int msgid=msgget(1000,IPC_CREAT|0600);
+    ERROR_CHECK(msgid,-1,"msgget");
+    struct msgbuf msgInfo;
+    bzero(&msgInfo,sizeof(msgInfo));
+    int ret=msgrcv(msgid,&msgInfo,sizeof(msgInfo.mtext),atoi(argv[1]),IPC_NOWAIT);
+    printf("errno=%d\n",errno);
+    ERROR_CHECK(ret,-1,"msgrcv");
+    printf("mtype=%ld,mtext=%s\n",msgInfo.mtype,msgInfo.mtext);
+    return 0;
+}
+
