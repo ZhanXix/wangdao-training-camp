c8a48a0ff9b8aa7b2fa37e412a9e1b87f14dda89
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-06-26 15:43:49 +0800

    Update msgget.c

--- Linux/teach/day9_msg/msgget.c
+++ Linux/teach/day9_msg/msgget.c
@@ -1,9 +1,17 @@
 #include <func.h>
-
+struct msgbuf{
+    long mtype;
+    char mtext[64];
+};
 int main(int argc,char *argv[])
 {
     int msgid=msgget(1000,IPC_CREAT|0600);
     ERROR_CHECK(msgid,-1,"msgget");
+    struct msgbuf msgInfo;
+    msgInfo.mtype=1;
+    strcpy(msgInfo.mtext,"hello");
+    int ret=msgsnd(msgid,&msgInfo,strlen(msgInfo.mtext),0);
+    ERROR_CHECK(ret,-1,"msgsnd");
     return 0;
 }
 
