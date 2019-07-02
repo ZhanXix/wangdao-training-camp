1e2e428865a7297826ed5d642c31c30304b2462b
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-07-02 17:01:49 +0800

    +epoll_tcp_chat

---
+++ Linux/teach/day14_epoll_tcp_chat/server/tcp_server.c
@@ -0,0 +1,58 @@
+#include "func.h"
+int tcpInit(int *, char *, char *);
+int main(int argc, char *argv[])
+{
+    ARGS_CHECK(argc, 3);
+    int socketFd;
+    int ret;
+    ret=tcpInit(&socketFd, argv[1], argv[2]);
+    if(-1==ret)
+    {
+        return -1;
+    }
+    int newFd = accept(socketFd, NULL, NULL);
+    char buf[10];
+    int epfd=epoll_create(1);
+    struct epoll_event event,evs[2];
+    event.events=EPOLLIN;//监听读事件
+    event.data.fd=STDIN_FILENO;
+    ret=epoll_ctl(epfd,EPOLL_CTL_ADD,STDIN_FILENO,&event);
+    ERROR_CHECK(ret,-1,"epoll_ctl");
+    event.data.fd=newFd;
+    ret=epoll_ctl(epfd,EPOLL_CTL_ADD,newFd,&event);
+    ERROR_CHECK(ret,-1,"epoll_ctl");
+    int readyFdcount,i;
+    while (1)
+    {
+        readyFdcount = epoll_wait(epfd,evs,2,-1);
+        for (i = 0; i < readyFdcount; i++)
+        {
+            if (evs[i].events==EPOLLIN&&evs[i].data.fd==STDIN_FILENO)
+            {
+                bzero(buf, sizeof(buf));
+                ret = read(STDIN_FILENO, buf, sizeof(buf));
+                if (0 == ret)
+                {
+                    printf("byebye\n");
+                    goto end;
+                }
+                send(newFd, buf, strlen(buf) - 1, 0);
+            }
+            if (evs[i].data.fd==newFd)
+            {
+                bzero(buf, sizeof(buf));
+                ret = recv(newFd, buf, sizeof(buf), 0);
+                if (0 == ret)
+                {
+                    printf("byebye\n");
+                    goto end;
+                }
+                printf("%s\n", buf);
+            }
+        }
+    }
+end:
+    close(newFd);
+    close(socketFd);
+    return 0;
+}
