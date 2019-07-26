927612732ceccbf0adf5f1cf9049edf388228e37
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-07-26 21:20:45 +0800

    Create 5_1_log4cpp_test.cc

---
+++ C++/homework/5_1_log4cpp_test.cc
@@ -0,0 +1,32 @@
+#include <log4cpp/Category.hh>
+#include <log4cpp/OstreamAppender.hh>
+#include <log4cpp/Priority.hh>
+#include <log4cpp/PatternLayout.hh>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    log4cpp::PatternLayout * ptLayout = new log4cpp::PatternLayout();
+    ptLayout->setConversionPattern("%d [%p] %c %x: %m%n");
+
+    log4cpp::OstreamAppender * osAppender = new log4cpp::OstreamAppender("osAppender", &cout);
+    osAppender->setLayout(ptLayout);
+
+    log4cpp::Category & root = log4cpp::Category::getRoot();
+    log4cpp::Category & myCategory = root.getInstance("myCategory");
+
+    myCategory.addAppender(osAppender);
+    myCategory.setPriority(log4cpp::Priority::DEBUG);
+
+    myCategory.info("I am info");
+    myCategory.warn("I am warn");
+    myCategory.error("I am error");
+    myCategory.fatal("I am fatal");
+    myCategory.debug("I am debug");
+    
+    return 0;
+}
+
