ccebe30de44f027db9152e0a40309a39c37d9355
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-07-29 16:53:21 +0800

    Update Complex3.cc

--- C++/teach/day7_operator_overload/Complex3.cc
+++ C++/teach/day7_operator_overload/Complex3.cc
@@ -5,9 +5,9 @@ using std::endl;
 class Complex
 {
 public:
-    Complex(double dreal, double dimag)
-    : _dreal(dreal)
-    , _dimag(dimag)
+    Complex(double dreal=0, double dimag=0)
+        : _dreal(dreal)
+          , _dimag(dimag)
     {
         cout << "Complex(double, double)" << endl;
     }
@@ -26,10 +26,11 @@ public:
     void setImag(double imag) { _dimag = imag; }
 
     //运算符重载之成员函数
+    //(不推荐使用)
     Complex operator+(const Complex &rhs)
     {
         return Complex(_dreal + rhs._dreal,
-                    _dimag + rhs._dimag);
+                       _dimag + rhs._dimag);
     }
 
 private:
@@ -44,13 +45,35 @@ private:
 //}
 
 
-int main()
+int test0()
 {
     Complex c1(1,2), c2(3,4);
     Complex c3 = c1 + c2;
     cout << "c3 = ";
     c3.display();
 
+    return 0;
+}
+
+void test1()
+{
+    Complex c1(1,2);
+    cout << "c1 = ";
+    c1.display();
+    Complex c2 = c1 + 5;//隐式转换
+        //c1.operator+(5)
+    cout << "c2 = ";
+    c2.display();
+    //c2 = 5 + c1;
+        //5.operator+(c1) //error
+    //cout << "c2 = ";
+    //c2.display();
+}
+
+
+int main()
+{
+    test1();
     return 0;
 }
 
