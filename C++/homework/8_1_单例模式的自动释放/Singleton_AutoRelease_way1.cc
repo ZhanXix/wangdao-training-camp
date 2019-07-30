07146abaeeed67c0ccd94d6c5f9706c91890a615
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-07-30 20:55:11 +0800

    Create Singleton_AutoRelease_way1.cc

---
+++ C++/homework/8_1_单例模式的自动释放/Singleton_AutoRelease_way1.cc
@@ -0,0 +1,65 @@
+//单例模式的自动释放
+//（方法1：嵌套类 + 静态对象）
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Sigleton
+{
+	class AutoRelease
+	{
+	public:
+		AutoRelease()
+		{
+			cout << "AutoRelease()" << endl;
+		}
+
+		~AutoRelease()
+		{
+			cout << "~AutoRelease()" << endl;
+			if (_pSigleton) {
+				delete _pSigleton;
+			}
+		}
+	};
+
+public:
+	static Sigleton* getInstance()
+	{
+		if (nullptr == _pSigleton){
+			_pSigleton = new Sigleton();
+		}
+		return _pSigleton;
+	}
+
+private:
+	Sigleton()
+	{
+		cout << "Sigleton()" << endl;
+	}
+
+	~Sigleton()
+	{
+		cout << "~Sigleton()" << endl;
+	}
+
+	//private data
+	static Sigleton* _pSigleton;
+	static AutoRelease _autoRelease;
+};
+
+Sigleton* Sigleton::_pSigleton = nullptr;
+Sigleton::AutoRelease Sigleton::_autoRelease;
+
+int main()//test
+{
+	Sigleton* pSig1 = Sigleton::getInstance();
+	Sigleton* pSig2 = Sigleton::getInstance();
+
+	cout << "pSig1 = " << pSig1 << endl;
+	cout << "pSig2 = " << pSig2 << endl;
+
+	return 0;
+}
+
