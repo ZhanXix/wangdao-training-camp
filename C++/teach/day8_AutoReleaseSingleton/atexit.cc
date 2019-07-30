c135e0482f71a82dcdc4f07a518a4c2f8cbb268f
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-07-30 21:00:00 +0800

    Update atexit.cc

--- C++/teach/day8_AutoReleaseSingleton/atexit.cc
+++ C++/teach/day8_AutoReleaseSingleton/atexit.cc
@@ -1,9 +1,11 @@
- ///
- /// @file    atexit.cc
- /// @author  lemon(haohb13@gmail.com)
- /// @date    2019-07-30 14:33:31
- ///
- 
+/*
+函数名: atexit
+头文件: #include<stdlib.h>
+功 能: 注册终止函数(即main执行结束后调用的函数)
+用 法: void atexit(void (*func)(void));
+注意：exit调用这些注册函数的顺序与它们 登记时候的顺序相反。同一个函数如若登记多次，则也会被调用多次。
+*/
+
 #include <stdlib.h>
 
 #include <iostream>
