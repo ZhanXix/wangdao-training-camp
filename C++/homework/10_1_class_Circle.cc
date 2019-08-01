4318fe72a0cd0c8949433803db52af62695e0289
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-01 21:21:51 +0800

    Update 10_1_class_Circle.cc

--- C++/homework/10_1_class_Circle.cc
+++ C++/homework/10_1_class_Circle.cc
@@ -38,17 +38,17 @@ public:
 	}
 
 	//成员方法
-	double getArea()      //获取圆的面积
+	double getArea() const     //获取圆的面积
 	{
 		return Pi * _r * _r;
 	}
 
-	double getPerimeter() //获取圆的周长
+	double getPerimeter() const	//获取圆的周长
 	{
 		return 2 * Pi * _r;
 	}
 
-	void show()          //将圆的半径、周长、面积输出到屏幕
+	void show() const         //将圆的半径、周长、面积输出到屏幕
 	{
 		cout << "radius = " << _r
 			<< ", perimeter = " << getPerimeter()
