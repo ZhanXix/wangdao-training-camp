0f4d1174125e01acd7fffc64e218d179f1bd80d7
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-01 21:09:16 +0800

    Create 10_2_class_Cylinder.cc

---
+++ C++/homework/10_2_class_Cylinder.cc
@@ -0,0 +1,112 @@
+/* 
+ * 编写一个圆柱体类Cylinder，它继承于上面的Circle类，还拥有：    
+ *	① 1个成员变量，圆柱体的高；    
+ *	② 构造方法               
+ *			Cylinder (double r, double  h) //创建Circle对象时将半径初始化为r             
+ *	③ 成员方法    
+ *			double getVolume( )   //获取圆柱体的体积                 
+ *			void  showVolume( )   //将圆柱体的体积输出到屏幕      
+ *		编写应用程序，创建类的对象，分别设置圆的半径、圆柱体的高，    
+ *		计算并分别显示圆半径、圆面积、圆周长，圆柱体的体积。    
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+constexpr double Pi = 3.1415926;
+
+class Circle
+{
+public:
+	//构造
+	Circle()
+		: _r(0)
+	{
+		cout << "Circle()" << endl;
+	}
+
+	Circle(double r)
+		: _r(r)
+	{
+		cout << "Circle(double)" << endl;
+	}
+
+	//析构
+	~Circle()
+	{
+		cout << "~Circle()" << endl;
+	}
+
+	//成员方法
+	double getArea()      //获取圆的面积
+	{
+		return Pi * _r * _r;
+	}
+
+	double getPerimeter() //获取圆的周长
+	{
+		return 2 * Pi * _r;
+	}
+
+	void show()          //将圆的半径、周长、面积输出到屏幕
+	{
+		cout << "radius = " << _r
+			<< ", perimeter = " << getPerimeter()
+			<< ", area = " << getArea() << endl;
+	}
+
+private:
+	double _r;	//圆的半径
+};
+
+class Cylinder
+	: public Circle
+{
+public:
+	//构造
+	Cylinder(double r, double h)
+		: Circle(r)
+		, _h(h)
+	{
+		cout << "Cylinder(double,double)" << endl;
+	}
+
+	//析构
+	~Cylinder()
+	{
+		cout << "~Cylinder()" << endl;
+	}
+
+	//成员方法
+	double getVolume()   //获取圆柱体的体积
+	{
+		return getArea() * _h;
+	}
+
+	void  showVolume()   //将圆柱体的体积输出到屏幕
+	{
+		cout << "volume = " << getVolume() << endl;
+	}
+
+private:
+	double _h;	//圆柱体的高
+};
+
+
+//测试代码
+int main()	
+{
+	Cylinder cl1(1, 1);
+	Cylinder cl2(3, 2);
+
+	cout << "Cylinder(1, 1) " << endl;
+	cl1.show();
+	cl1.showVolume();
+
+	cout << "Cylinder(3, 2) " << endl;
+	cl2.show();
+	cl2.showVolume();
+
+	return 0;
+}
