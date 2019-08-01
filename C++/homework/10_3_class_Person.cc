a95f8102af80d06cd806288b3f71083dbfec25fa
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-01 21:18:44 +0800

    Create 10_3_class_Person.cc

---
+++ C++/homework/10_3_class_Person.cc
@@ -0,0 +1,29 @@
+/* 
+ *	构建一个类person，包含字符串成员name（姓名），整型数据成员age（年龄），
+	成员函数display（）用来输出name和age。构造函数包含两个参数，用来对name和age初始化。    
+ *	构建一个类employee由person派生，包含department（部门），实型数据成员salary（工资）, 
+	成员函数display（）用来输出职工姓名、年龄、部门、工资，其他成员根据需要自己设定。    
+ *	主函数中定义3个employee类对象，内容自己设定，
+	将其姓名、年龄、部门、工资输出，并计算他们的平均工资。
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Circle
+{
+public:
+	
+
+private:
+	
+};
+
+
+//测试代码
+int main()	
+{
+
+	return 0;
+}
