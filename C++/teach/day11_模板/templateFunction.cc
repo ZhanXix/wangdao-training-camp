f6b4da19188063cf82cb74cc96de418dd2c272e9
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-05 15:29:54 +0800

    Update templateFunction.cc

--- C++/teach/day11_模板/templateFunction.cc
+++ C++/teach/day11_模板/templateFunction.cc
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -20,7 +21,7 @@ T add(T x, T y, T z)
 //普通函数与函数模板是可以进行重载的
 //而且普通函数的执行要优先于函数模板
 //
-#if 1
+#if 0
 int add(int x, int y)
 {
 	cout << "int add(int,int)" << endl;
@@ -28,11 +29,22 @@ int add(int x, int y)
 }
 #endif
 
+//模板的特化(specialization)
+template<>
+const char* add<const char*>(const char* pstr1, const char* pstr2)
+{
+	char* ptmp = new char[strlen(pstr1) + strlen(pstr2) + 1]();
+	strcpy(ptmp, pstr1);
+	strcat(ptmp, pstr2);
+	return ptmp;
+}
+
 int main()
 {
 	int d1 = 1, d2 = 2;
 	double d3 = 1.1, d4 = 2.2;
 	char d5 = 'a', d6 = 1;
+
 	cout << "add(d1, d2) = " << add(d1, d2) << endl;//隐式实例化
 	cout << "add(d3, d4) = " << add<double>(d3, d4) << endl;//显式实例化
 	cout << "add(d5, d6) = " << add(d5, d6) << endl;
@@ -40,12 +52,16 @@ int main()
 	int d7 = 3;
 	cout << "add(d1, d2, d7) = " << add(d1, d2, d7) << endl;
 
+	const char* p1 = "hello";
+	const char* p2 = "world";
+	cout << "add(p1, p2) = " << add(p1, p2) << endl;
+
 	return 0;
 }
 
 template <class T>
 T add(T x, T y)//函数模板的实现
 {
-	cout << "T add(T x, T y)" << endl;
+	//cout << "T add(T x, T y)" << endl;
 	return x + y;
 }
