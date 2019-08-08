0d7bc13ecc12d93405cf2c3c0ad7d0d5a965b9f5
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-08 10:04:27 +0800

    Create TemplateSingleton.cc

---
+++ C++/teach/day15_作业范例/TemplateSingleton.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template <class T>
+class Singleton
+{
+	class AutoRelease
+	{
+	public:
+		AutoRelease() { cout << "AutoRelease()" << endl; }
+		~AutoRelease()
+		{
+			if (_pInstance) {
+				delete _pInstance;
+			}
+			cout << "~AutoRelease()" << endl;
+		}
+	};
+public:
+	template <class... Args>
+	static T* getInstance(Args... args)
+	{
+		if (_pInstance == nullptr) {
+			_pInstance = new T(args...);
+		}
+		return _pInstance;
+	}
+
+private:
+	Singleton() { cout << "Singleton()" << endl; }
+	~Singleton() { cout << "~Singleton()" << endl; }
+
+private:
+	static T* _pInstance;
+	static AutoRelease _auto;
+};
+
+template<class  T>
+T* Singleton<T>::_pInstance = nullptr;
+
+template<class  T>
+typename Singleton<T>::AutoRelease Singleton<T>::_auto;
+
+class Point
+{
+public:
+	Point(int ix, int iy)
+		: _ix(ix)
+		, _iy(iy)
+	{
+		cout << "Point(int, int)" << endl;
+	}
+
+	~Point() { cout << "~Point()" << endl; }
+
+private:
+	int _ix;
+	int _iy;
+};
+
+int main()
+{
+	Point* pt = Singleton<Point>::getInstance(1, 2);
+	Point* pt2 = Singleton<Point>::getInstance(1, 2);
+
+	cout << "pt = " << pt << endl;
+	cout << "pt2 = " << pt2 << endl;
+
+	return 0;
+}
