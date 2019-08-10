8f4eece9b4b56200f209d33c360d1711718bf3ae
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-10 10:55:09 +0800

    Update String.cc

--- C++/teach/day17/String.cc
+++ C++/teach/day17/String.cc
@@ -22,6 +22,9 @@ public:
 	}
 
 	//当const左值引用作为一个形参，无法区分出来传递过来的实参是左值还是右值
+	//
+	//具有复制控制语义的函数
+	//复制构造函数
 	String(const String& rhs)
 		: _pstr(new char[strlen(rhs._pstr) + 1]())
 	{
@@ -29,11 +32,35 @@ public:
 		strcpy(_pstr, rhs._pstr);
 	}
 
+	//赋值运算符函数
+	String& operator=(const String& rhs)
+	{
+		cout << "String& operator=(const String&)" << endl;
+		if (this != &rhs) {
+			delete[] _pstr;
+			_pstr = new char[strlen(rhs._pstr) + 1]();
+			strcpy(_pstr, rhs._pstr);
+		}
+		return *this;
+	}
+
+	//具有移动语义的函数，执行要优先于具有复制控制语义函数的执行（使用右值时就调移动语义的函数）
+	//移动构造函数
 	String(String&& rhs)
 		: _pstr(rhs._pstr) //使用浅拷贝，完成转移
 	{
 		cout << "String(String&&)" << endl;
 		rhs._pstr = nullptr;
+	}
+
+	//移动赋值函数
+	String& operator=(String&& rhs)
+	{
+		cout << "String& operator=(String&&)" << endl;
+		delete[] _pstr;
+		_pstr = rhs._pstr;
+		rhs._pstr = nullptr;
+		return *this;
 	}
 
 	~String()
@@ -56,10 +83,48 @@ std::ostream& operator<<(std::ostream& os, const String& rhs)
 	return os;
 }
 
-int main()
+String str("hello,world");
+
+//当函数的返回值是对象时：如果返回的对象是一个
+//局部（即将被销毁的）对象，执行return语句时，
+//会调用移动构造函数；当返回的对象其声明周期是
+//大于函数时，执行return语句时，会调用复制构造
+//函数
+String getString()
+{
+	String str("hello,world");
+	cout << "strs =" << str << endl;
+	return str;
+}
+
+int test0()
 {
 	vector<String> strs;
 	strs.push_back("hello,world");
+	cout << "strs[0] = " << strs[0] << endl << endl;
 
+	String s1 = "hello";
+	s1 = String("world");
+
+	String s2 = "shenzhen";
+	cout << "s2 = " << s2 << endl;
+	//&s2;
+	s1 = std::move(s2);//显式将一个左值转换成右值
+						//一般表示在该语句之后，不会再使用该对象
+	cout << "s1 = " << s2 << endl;
+	//cout << "s2 = " << s2 << endl;//error
+	cout << "......" << endl;
+
+	return 0;
+}
+
+void test1()
+{
+	getString();
+}
+
+int main()
+{
+	test1();
 	return 0;
 }
