78528b1ecb67d71b3c77249c2942879c5673b9fe
夕湛 <30312927+ZhanXix@users.noreply.github.com>
2019-08-10 10:04:12 +0800

    Update String.cc

--- C++/teach/day17/String.cc
+++ C++/teach/day17/String.cc
@@ -21,11 +21,19 @@ public:
 		strcpy(_pstr, pstr);
 	}
 
+	//当const左值引用作为一个参数，无法区分出来传递过来的实参是左值还是右值
 	String(const String& rhs)
 		: _pstr(new char[strlen(rhs._pstr) + 1]())
 	{
 		cout << "String(const String&)" << endl;
 		strcpy(_pstr, rhs._pstr);
+	}
+
+	String(String&& rhs)
+		: _pstr(rhs._pstr) //使用浅拷贝，完成转移
+	{
+		cout << "String(String&&)" << endl;
+		rhs._pstr = nullptr;
 	}
 
 	~String()
